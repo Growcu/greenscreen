2aae087f02fb504a6e10a8abaf8b1b428201be72
Krystian Zasada (kz300505) <kz300505@student.polsl.pl>
2023-11-30 23:21:38 +0100

    Initialize project

---
+++ green-screen-ja/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace green_screen_ja
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+
+        [STAThread]
+        [DllImport(@"C:\projekty\green-screen-ja\x64\Debug\JAAsm.dll")]
+        static extern int MyProc1(int a, int b);
+        static void Main()
+        {
+
+            int x = 5, y = 3;
+            int retVal = MyProc1(x, y);
+            Console.Write("Moja pierwsza wartość obliczona w asm to:");
+            Console.WriteLine(retVal);
+            Console.ReadLine();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form form = new Form();
+            Label label1 = new Label();
+            label1.Width = 500;
+            label1.Text = $"Moja pierwsza wartość obliczona w asm to: {retVal}";
+            form.Controls.Add(label1);
+            Application.Run(form);
+
+        }
+    }
+}
